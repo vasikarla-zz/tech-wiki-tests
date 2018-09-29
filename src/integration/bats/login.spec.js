da5bada38886388e7eaea5e4a24da32882b6f69d
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-09-29 11:18:36 -0700

    adding login related tests

---
+++ src/integration/bats/login.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+
+context('Login Test', () => {
+
+    it('Should be able to login with valid credentials', () => {
+        cy.visit("https://tech.wiki/login/");
+        cy.get("#username-117").type("rajvasikarla");
+        cy.get("#user_password-117").type("splendor");
+        cy.get("#um-submit-btn").click();      
+    })
+  
+    it('Should be able to login with valid credentials', () => {
+        cy.visit("https://tech.wiki/login/");
+        cy.get("#username-117").type("rajvasikarlaa");
+        cy.get("#user_password-117").type("splendor");
+        cy.get("#um-submit-btn").click();      
+
+        cy.get('.um-notice').should('be.visible');
+        cy.get('.um-field-username > .um-field-error').should("be.visible");
+        cy.get('.um-field-user_password > .um-field-error').should("be.visible");
+
+    })
+
+  })
